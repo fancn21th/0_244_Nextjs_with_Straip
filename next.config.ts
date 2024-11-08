17056c9cb9e54fd3a7a044568054acd4cd1679f7
Fan, Yi-Jie <fancn21th@aliyun.com>
2024-11-08 15:40:06 +0800

    📦 NEW: product card

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "10.0.5.68",
+        port: "1337",
+        pathname: "/uploads/**/*",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,43 @@
+import Link from "next/link";
+import Image from "next/image";
 import qs from "qs";
+
+interface ProductCardProps {
+  name: string;
+  description: string;
+  picture: [
+    {
+      alternativeText: string;
+      name: string;
+      url: string;
+    }
+  ];
+}
+
+function ProductCard({
+  name,
+  description,
+  picture,
+}: Readonly<ProductCardProps>) {
+  const imageUrl = `${
+    process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:1337"
+  }${picture[0].url}`;
+
+  return (
+    <Link href={`#`} className="bg-white rounded-lg shadow-md overflow-hidden">
+      <Image
+        src={imageUrl}
+        alt={picture[0].alternativeText || name}
+        width={500}
+        height={500}
+      />
+      <div className="p-6">
+        <h3 className="text-xl font-semibold mb-2">{name}</h3>
+        <p className="text-gray-600">{description}</p>
+      </div>
+    </Link>
+  );
+}
 
 async function getProducts() {
   const baseUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:1337";
@@ -29,7 +68,12 @@ export default async function Home() {
 
   return (
     <div>
-      <pre>{JSON.stringify(products, null, 2)}</pre>
+      <h1 className="text-3xl font-bold mb-8">Products</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {products.data.map((product: ProductCardProps) => (
+          <ProductCard key={product.name} {...product} />
+        ))}
+      </div>
     </div>
   );
 }
