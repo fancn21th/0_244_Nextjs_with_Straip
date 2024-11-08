7c2e9ace4413c4a339b7188d5ebf2da09a1fcd40
Fan, Yi-Jie <fancn21th@aliyun.com>
2024-11-08 15:52:48 +0800

    👌 IMPROVE: bit more elegant style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,8 +67,9 @@ export default async function Home() {
   const products = await getProducts();
 
   return (
-    <div>
+    <div className="m-4">
       <h1 className="text-3xl font-bold mb-8">Products</h1>
+      <hr className="mb-8" />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {products.data.map((product: ProductCardProps) => (
           <ProductCard key={product.name} {...product} />
